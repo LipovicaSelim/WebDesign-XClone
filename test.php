ab1f60714649f309e269873650f6dcbfa48335e3
ademajd <drilonademaj@gmail.com>
2024-02-02 20:58:24 +0100

    deleted files

--- test.php
+++
@@ -1,9 +0,0 @@
- <?php
-include_once("./helpers/AccountRepository.php");
-include_once("./helpers/TweetRepository.php");
-// var_dump(__DIR__);
-$tweetRep = new TweetRepository();
-$tweets = $tweetRep->getAllTweetsOfFollowed(1);
-$acRepo = new AccountRepository();
-
-var_dump($d = $acRepo->getProfilePicture(1));
